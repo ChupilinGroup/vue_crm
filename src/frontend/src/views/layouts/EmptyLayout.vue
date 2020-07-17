f9780ad288e0738e32862922fcf286f072eff095
domovikx <domovikx@gmail.com>
2020-07-17 14:50:18 +0300

    HotFix

--- src/frontend/src/views/layouts/EmptyLayout.vue
+++ src/frontend/src/views/layouts/EmptyLayout.vue
@@ -4,8 +4,6 @@ import Vue from 'vue';
 import SnackbarComponent from '../../components/SnackbarComponent.vue';
 import LoaderComponent from '../../components/LoaderComponent.vue';
 
-import LoaderComponent from '../../components/LoaderComponent.vue';
-
 export default Vue.extend({
   name: 'EmptyLayout',
 
